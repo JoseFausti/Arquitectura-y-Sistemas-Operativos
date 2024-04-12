50dd5b887ac1dbcb413f77388fd988ae6b47e700
JoseFausti <joseemilianofausti@gmail.com>
2024-04-11 21:31:21 -0300

    new

--- src/Clase_3/Ejercicio_2_1/Proceso.java
+++ src/Clase_3/Ejercicio_2_1/Proceso.java
@@ -20,30 +20,28 @@ public class Proceso extends Thread{
         }
     }
 
-    public class Main {
-        public static void main(String[] args) {
-            Random rand = new Random(System.nanoTime());
-            for (int i = 0; i < vec.length; i++) {
-                vec[i] = rand.nextInt(10);
-                System.out.println(vec[i] + " ");
-            }
-            Proceso p1 = new Proceso(0,4);
-            Proceso p2 = new Proceso(4,8);
 
-            p1.start();
-            p2.start();
+    public static void main(String[] args) {
+        Random rand = new Random(System.nanoTime());
+        for (int i = 0; i < vec.length; i++) {
+            vec[i] = rand.nextInt(10);
+            System.out.println(vec[i] + " ");
+        }
+        Proceso p1 = new Proceso(0,4);
+        Proceso p2 = new Proceso(4,8);
 
-            try {
-                p1.join();
-                p2.join();
-            }catch (InterruptedException e){
-                throw new RuntimeException(e);
-            }
+        p1.start();
+        p2.start();
 
-            for (int i = 0; i < vec.length; i++) {
-                System.out.println(vec[i] + " ");
-            }
+        try {
+            p1.join();
+            p2.join();
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+
+        for (int i = 0; i < vec.length; i++) {
+            System.out.println(vec[i] + " ");
         }
     }
-
 }
